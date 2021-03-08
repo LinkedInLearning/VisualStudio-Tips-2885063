6258222bbb79cca24deec476298db0094da2f1fd
walt-instructor <staffauthor@outlook.com>
2021-03-07 17:24:38 -0800

    Add duplicate demo method

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -11,12 +11,18 @@ namespace TipsConsole
 		}
 		static void AddKeyboardShortcut()
 		{
-			Console.WriteLine("Here's a useful tip");
-			Console.WriteLine("for developers that like keyboard shortcuts.");
+
 			Console.WriteLine("Add a keyboard shortcut");
 			Console.WriteLine("to do a Git pull operation");
 
 
+		}
+		public void ShowDuplicateTool()
+		{
+			Console.WriteLine("One");
+			Console.WriteLine("Two");
+			Console.WriteLine("Three");
+			Console.WriteLine("Four");
 		}
 	}
 }
