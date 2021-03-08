4a98dc094231e407bf4912f71349bf9653bced3f
walt-instructor <staffauthor@outlook.com>
2021-03-07 17:05:41 -0800

    add code for duplicate demo

--- source/TipsConsole/Program.cs
+++ source/TipsConsole/Program.cs
@@ -7,12 +7,13 @@ namespace TipsConsole
 		static void Main(string[] args)
 		{
 			
-
+			// a comment that explains something.
 		}
 		static void AddKeyboardShortcut()
 		{
 			Console.WriteLine("Add a keyboard shortcut");
 			Console.WriteLine("to do a Git pull operation");
+
 		}
 	}
 }
